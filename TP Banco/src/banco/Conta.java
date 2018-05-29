d9bb3c79e7eeb70afb3448353f11831a4a526ef2
Davi Rolim <davi.delima@hotmail.com>
2018-05-29 18:03:47 -0300

    Classe Conta criada

---
+++ TP Banco/src/banco/Conta.java
@@ -0,0 +1,18 @@
+package banco;
+
+public class Conta {
+	private int numConta;
+	private double saldo;
+	private Cliente cliente;
+	public int getNumConta() {
+		return numConta;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+
+}
